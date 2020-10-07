2b6719c998bf2589312f9e77423e7ef36fa47ad9
Brent Mullen <brent.mullen@bankrate.com>
2020-10-07 15:48:58 -0400

    feat: rewrite logic to pull in PageSpeed Insights v6 data
    - Pulls 6 key factors
    - Calculates score based on the Lighthouse Scoring Calculator (https://googlechrome.github.io/lighthouse/scorecalc/)
    - **Only run mobile, ignores desktop**

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 2. Queries each url with the google pagespeed api.
 3. Filters JSON results to only include desired metrics.
 4. Metrics are saved to local .csv spreadsheet for analysis.
+
+### Reqs
+Python3
+
+### Google API Key
+Create API Key: https://console.developers.google.com/apis/credentials (Create Credentials)
+
+Enable PageSpeed Insights API: https://console.developers.google.com/apis/api/pagespeedonline.googleapis.com/overview
+
+```bash
+export GOOGLE_API_KEY={YOUR_KEY}
+```
--- pagespeed-api.py
+++ pagespeed-api.py
@@ -1,54 +1,78 @@
-import requests
+#!/usr/bin/env python3
+import requests, os, csv
 
 # Documentation: https://developers.google.com/speed/docs/insights/v5/get-started
 
 # JSON paths: https://developers.google.com/speed/docs/insights/v4/reference/pagespeedapi/runpagespeed
+googleApiKey = os.getenv('GOOGLE_API_KEY')
+
+if googleApiKey is None:
+    googleApiKey = input('Google API Key (can be empty for single run): ')
 
 # Populate 'pagespeed.txt' file with URLs to query against API.
-with open('pagespeed.txt') as pagespeedurls:
+with open('pagespeed.csv') as pagespeedurls:
     download_dir = 'pagespeed-results.csv'
     file = open(download_dir, 'w')
-    content = pagespeedurls.readlines()
-    content = [line.rstrip('\n') for line in content]
-
-    columnTitleRow = "URL, First Contentful Paint, First Interactive\n"
+    columnTitleRow = "Description, URL, V6 Score, FCP, SI, LCP, TTI, TBT, CLS\n"
     file.write(columnTitleRow)
 
+    content = csv.reader(pagespeedurls, delimiter=',')
+
     # This is the google pagespeed api url structure, using for loop to insert each url in .txt file
-    for line in content:
+    for row in content:
+        if row[0] == 'Description':
+            continue
+
         # If no "strategy" parameter is included, the query by default returns desktop data.
-        x = f'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url={line}&strategy=mobile'
+        analyzeUrl = row[1].replace('&', '%26').strip(' ')
+        x = f'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url={analyzeUrl}&strategy=mobile'
         print(f'Requesting {x}...')
-        r = requests.get(x)
+        r = requests.get(f'{x}&key={googleApiKey}')
         final = r.json()
         
         try:
-            urlid = final['id']
-            split = urlid.split('?') # This splits the absolute url from the api key parameter
-            urlid = split[0] # This reassigns urlid to the absolute url
-            ID = f'URL ~ {urlid}'
-            ID2 = str(urlid)
-            urlfcp = final['lighthouseResult']['audits']['first-contentful-paint']['displayValue']
-            FCP = f'First Contentful Paint ~ {str(urlfcp)}'
-            FCP2 = str(urlfcp)
-            urlfi = final['lighthouseResult']['audits']['interactive']['displayValue']
-            FI = f'First Interactive ~ {str(urlfi)}'
-            FI2 = str(urlfi)
+            url = str(final['id'])
+
+            fcp = str(final['lighthouseResult']['audits']['first-contentful-paint']['displayValue'])
+            fcpScore = final['lighthouseResult']['audits']['first-contentful-paint']['score']
+
+            si = str(final['lighthouseResult']['audits']['speed-index']['displayValue'])
+            siScore = final['lighthouseResult']['audits']['speed-index']['score']
+
+            lcp = str(final['lighthouseResult']['audits']['largest-contentful-paint']['displayValue'])
+            lcpScore = final['lighthouseResult']['audits']['largest-contentful-paint']['score']
+
+            tti = str(final['lighthouseResult']['audits']['interactive']['displayValue'])
+            ttiScore = final['lighthouseResult']['audits']['interactive']['score']
+
+            tbt = str(final['lighthouseResult']['audits']['total-blocking-time']['displayValue'])
+            tbtScore = final['lighthouseResult']['audits']['total-blocking-time']['score']
+
+            cls = str(final['lighthouseResult']['audits']['cumulative-layout-shift']['displayValue'])
+            clsScore = final['lighthouseResult']['audits']['cumulative-layout-shift']['score']
+
+            v6score = round(((fcpScore * .15) + (siScore * .15) + (lcpScore * .25) + (ttiScore * .15) + (tbtScore * .25) + (clsScore * .05)) * 100)
         except KeyError:
-            print(f'<KeyError> One or more keys not found {line}.')
+            print(f'<KeyError> One or more keys not found {analyzeUrl}.')
         
         try:
-            row = f'{ID2},{FCP2},{FI2}\n'
+            row = f'{row[0]}, {url}, {str(v6score)}, {fcp}, {si}, {lcp}, {tti}, {tbt}, {cls}\n'.replace(' ', ' ')
             file.write(row)
         except NameError:
-            print(f'<NameError> Failing because of KeyError {line}.')
-            file.write(f'<KeyError> & <NameError> Failing because of nonexistant Key ~ {line}.' + '\n')
+            print(f'<NameError> Failing because of KeyError {analyzeUrl}.')
+            file.write(f'<KeyError> & <NameError> Failing because of nonexistant Key ~ {analyzeUrl}.' + '\n')
         
         try:
-            print(ID) 
-            print(FCP)
-            print(FI)
+            print(f'Description: {row[0]}')
+            print(f'URL: {url}')
+            print(f'Mobile Score (V6): {str(v6score)}')
+            print(f'First Contentful Paint: {fcp} ({str(fcpScore)})')
+            print(f'Speed Index: {si} ({str(siScore)})')
+            print(f'Largest Contentful Paint: {lcp} ({str(lcpScore)})')
+            print(f'Time to Interactive: {tti} ({str(ttiScore)})')
+            print(f'Total Block Time: {tbt} ({str(tbtScore)})')
+            print(f'Cumulative Layout Shift: {cls} ({str(clsScore)})')
         except NameError:
-            print(f'<NameError> Failing because of KeyError {line}.')
+            print(f'<NameError> Failing because of KeyError {analyzeUrl}.')
 
     file.close()
